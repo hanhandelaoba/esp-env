4b38ccaaf472933c7056c3cafed8fe79a2fd1223
wangkai <18964368607@189.cn>
2019-01-04 16:03:06 +0800

    update setespenv.sh to run docker container without ttyUSB0 device.

--- setespenv.sh
+++ setespenv.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-docker run --name esp32 --rm -ti --device /dev/ttyUSB0:/dev/ttyUSB0 -v `pwd`:/opt/esp/projects esp-env:v1.3 /bin/bash
+if [ "$1" = "d" ]
+then
+	docker run --name esp32 --rm -ti --device /dev/ttyUSB0:/dev/ttyUSB0 -v `pwd`:/opt/esp/projects esp-env:v1.3 /bin/bash
+else
+	docker run --name esp32 --rm -ti -v `pwd`:/opt/esp/projects esp-env:v1.3 /bin/bash
+fi
+
